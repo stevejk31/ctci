7874bb01dd5c0d6facf165c9b82031ec8cba1e7c
stevejk31 <stevekim31@gmail.com>
2016-04-22 10:21:17 -0700

    push

--- three_in_one.rb
+++ three_in_one.rb
@@ -1,15 +1,16 @@
 # descibe hwo you can use a single array to implement three stacks
 class ThreeInOne
-  def initialize(size, count)
+  def initialize(length, count)
     @stack = Array.new(size * count) {nil}
     @location = Array.new(count)
     count.times do |num|
       @location[num] = size * num
     end
+    @length = length
   end
 
-  def push(stack)
-    
+  def push(stack, value)
+    @stack[@location[stack-1]] = value
   end
 
   def pop(stack)
@@ -18,5 +19,9 @@ class ThreeInOne
 
   def peak(stack)
 
+  end
+
+  def max?(stack)
+    
   end
 end
