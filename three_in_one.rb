202fbbb8def432f3f0160e0a3b8338cf10bb4cfb
stevejk31 <stevekim31@gmail.com>
2016-04-22 10:31:51 -0700

    check max

--- three_in_one.rb
+++ three_in_one.rb
@@ -1,16 +1,20 @@
 # descibe hwo you can use a single array to implement three stacks
 class ThreeInOne
+  attr_reader :stack, :location, :length
   def initialize(length, count)
-    @stack = Array.new(size * count) {nil}
+    @stack = Array.new(length * count) {nil}
     @location = Array.new(count)
     count.times do |num|
-      @location[num] = size * num
+      @location[num] = length * num
     end
     @length = length
   end
-
+# 1st 2nd 3rd stack
   def push(stack, value)
+    #valid stack
+    raise "Max Stack" if max?(stack)
     @stack[@location[stack-1]] = value
+    @location[stack-1] +=1
   end
 
   def pop(stack)
@@ -22,6 +26,7 @@ class ThreeInOne
   end
 
   def max?(stack)
-    
+    require 'byebug'; debugger
+    @location[stack-1] >= @length * stack
   end
 end
