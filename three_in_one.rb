5d0a975702a7bfcbad4252564e0a491df750bd7a
stevejk31 <stevekim31@gmail.com>
2016-04-22 10:39:05 -0700

    empty, peak, valid_stack, pop

--- three_in_one.rb
+++ three_in_one.rb
@@ -11,22 +11,32 @@ class ThreeInOne
   end
 # 1st 2nd 3rd stack
   def push(stack, value)
-    #valid stack
+    raise "Not a Valid Stack" unless valid_stack?(stack)
     raise "Max Stack" if max?(stack)
     @stack[@location[stack-1]] = value
     @location[stack-1] +=1
   end
 
   def pop(stack)
-
+    raise "Not a Valid Stack" unless valid_stack?(stack)
+    raise "No Output" if empty?(stack)
+    @location[stack-1] -=1
+    @stack[@location[stack-1]]
   end
 
   def peak(stack)
 
   end
 
+  def valid_stack?(stack)
+    stack > 0 && stack <= @length
+  end
+
+  def empty?(stack)
+    @location[stack-1] <= @length * (stack-1)
+  end
+
   def max?(stack)
-    require 'byebug'; debugger
     @location[stack-1] >= @length * stack
   end
 end
