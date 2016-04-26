01ca1dd33c0bdac27bbb4b8401de1a8087e14c1f
stevejk31 <stevekim31@gmail.com>
2016-04-25 18:55:43 -0700

    complete

--- stack_queue.rb
+++ stack_queue.rb
@@ -12,12 +12,13 @@ class StackQueue
 
   def dequeue
     flip if @de_stack.empty?
+    raise 'Empty Queue' if @de_stack.empty?
     @de_stack.pop
   end
 
   def flip
     until @en_stack.empty?
-      de_stack.push(@en_stack.pop)
+      @de_stack.push(@en_stack.pop)
     end
   end
 end
