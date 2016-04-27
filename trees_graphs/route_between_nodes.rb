121b8461febb8d1218d51fc28d5d01618adf7336
stevejk31 <stevekim31@gmail.com>
2016-04-27 16:57:47 -0700

    dfs or bfs

--- trees_graphs/route_between_nodes.rb
+++ trees_graphs/route_between_nodes.rb
@@ -1,4 +1,9 @@
-class RouteBetweenNodes
-  def initialize()
-  end
+# Given a directed graph, design an algorithm to find out whether there is a
+# route between two nodes
+def route_between_nodes_dfs(node, end_node)
+  
+end
+
+def route_between_nodes_bfs(start_node, end_node)
+
 end
