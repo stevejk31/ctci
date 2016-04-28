3b79e5af047a3ed70f5ad72cd4c39af4fd3726d9
stevejk31 <stevekim31@gmail.com>
2016-04-27 17:00:43 -0700

    dfs

--- trees_graphs/route_between_nodes.rb
+++ trees_graphs/route_between_nodes.rb
@@ -1,9 +1,15 @@
 # Given a directed graph, design an algorithm to find out whether there is a
 # route between two nodes
-def route_between_nodes_dfs(node, end_node)
-  
+def route_between_nodes_dfs(node, end_node, route = [])
+  return false if node == nil
+  route.push(node)
+  children = node.children
+  children.each do |child|
+    return route if child == end_node
+    route_between_nodes_dfs(child, end_node, route)
+  end
 end
 
 def route_between_nodes_bfs(start_node, end_node)
-
+  
 end
