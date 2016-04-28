4e2a185d95ae10df342abecca3c7d91ed28b7d7b
stevejk31 <stevekim31@gmail.com>
2016-04-27 17:20:15 -0700

    minimal tree

---
+++ trees_graphs/minimal_tree.rb
@@ -0,0 +1,9 @@
+# Given a sorted (increasing order) array with unique integer elements, write an
+# algorithm to create a binary search tree with minimal height.
+
+
+class MinimalTree
+  def initialize
+  end
+
+end
