0b3bb23d3883de53a7fe627e82674ce9e91d7d9b
stevejk31 <stevekim31@gmail.com>
2016-04-27 17:15:17 -0700

    filter common

--- trees_graphs/route_between_nodes.rb
+++ trees_graphs/route_between_nodes.rb
@@ -1,15 +1,34 @@
 # Given a directed graph, design an algorithm to find out whether there is a
 # route between two nodes
-def route_between_nodes_dfs(node, end_node, route = [])
-  return false if node == nil
+def route_between_nodes_dfs(node, end_node, visited_nodes = [])
+  return true if node == end_node
   route.push(node)
   children = node.children
-  children.each do |child|
-    return route if child == end_node
-    route_between_nodes_dfs(child, end_node, route)
+  unique_children = []
+  unique_children = filter_common(children, visited_nodes)
+  visited_nodes.concat(unique_children)
+  return false if unique_children.empty?
+  unique_children.each do |child|
+    route_between_nodes_dfs(child, end_node, children)
   end
 end
 
 def route_between_nodes_bfs(start_node, end_node)
-  
+
+end
+
+
+def filter_common(new_array, old_array)
+  new_el = Hash.new { |hash, key| hash[key] = 0 }
+  result = []
+  new_array.each do |el|
+    new_el[el] += 1
+  end
+  old_array.each do |el|
+    new_el[el] -= 1
+  end
+  new_el.each do |el, count|
+    result.push(el) if count > 0
+  end
+  result
 end
