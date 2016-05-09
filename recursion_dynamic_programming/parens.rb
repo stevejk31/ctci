fc23735d9e7d275bf98623af223e35c4f1108ae6
stevejk31 <stevekim31@gmail.com>
2016-05-09 12:26:59 -0700

    parens solved

--- recursion_dynamic_programming/parens.rb
+++ recursion_dynamic_programming/parens.rb
@@ -3,4 +3,22 @@
 
 # EXAMPLE
 # Input: 3
-# Output: ((())), (()()), (())(), ()()()
+# Output: ((())), (()()), (())(), ()()(), ()(())
+
+# EXAMPLE
+# Input: 2
+# Output: (()), ()()
+
+def parens(num_parens)
+  return ["()"] if num_parens == 1
+  result = []
+  parens(num_parens - 1).each do |paren|
+    paren.insert(0,"(")
+    (paren.length-1).times do |num|
+      temp = paren.dup
+      temp.insert(num+1, ")")
+      result.push(temp) unless result.include?(temp)
+    end
+  end
+  result
+end
