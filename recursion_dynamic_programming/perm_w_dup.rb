79ef8ba9a5f458e0941a48d71463c90c2f7afde3
stevejk31 <stevekim31@gmail.com>
2016-05-09 11:48:30 -0700

    permutation with dulicate letters

--- recursion_dynamic_programming/perm_w_dup.rb
+++ recursion_dynamic_programming/perm_w_dup.rb
@@ -1,2 +1,23 @@
 # Write a method to comput all permutatiosn of a string whose charac-
 # ters are not necessarily unique. The list of permutations should not have duplicates.
+
+
+def get_perms(input_string, cache={})
+  return [""] if input_string.length == 0
+  if input_string.length == 1
+    return [input_string]
+    cache[input_string] = [input_string]
+  end
+  perms = []
+  input_string.split("").each_with_index do |char, idx|
+    temp_string = input_string[0...idx] + input_string[(idx+1)..-1]
+    unless cache[temp_string]
+      temp_perms = get_perms(temp_string, cache)
+      perms += temp_perms.map do |perm|
+        char+perm
+      end
+      cache[input_string] = perms.uniq!
+    end
+  end
+  perms
+end
--- recursion_dynamic_programming/perm_wo_dup.rb
+++ recursion_dynamic_programming/perm_wo_dup.rb
@@ -17,16 +17,9 @@ def get_perms(input_string)
   perms
 end
 
-def get_perm_two(two_string)
-  string_one = two_string[1]+two_string[0]
-  string_two = two_string[0]+two_string[1]
-  [string_one, string_two]
-end
-
 def get_perms_better(input_string)
   return [""] if input_string.length == 0
   return [input_string] if input_string.length == 1
-  return get_perm_two(input_string) if input_string.length == 2
   perms = []
   input_string.split("").each do |char|
     temp_string = input_string.delete(char)
