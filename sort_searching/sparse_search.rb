b9f9c74c27ffcd7f70825ff31349364b6211eda8
stevejk31 <stevekim31@gmail.com>
2016-05-15 16:26:09 -0700

    start sparse search

---
+++ sort_searching/sparse_search.rb
@@ -0,0 +1,16 @@
+# Given a sorted array of strings that is interspersed with empty strings,
+# write a method to find the location of a given string.
+
+EXAMPLE:
+Input: ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
+Output: 4
+
+def sparse_search(array, target)
+
+
+end
+
+
+def search(array, target)
+  array
+end
