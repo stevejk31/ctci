ae7d38b289162c4934c23e57dd6cdcd03c3a9658
stevejk31 <stevekim31@gmail.com>
2016-05-17 14:43:30 -0700

    start better peaks and valleys solution

--- sort_searching/peaks_valleys.rb
+++ sort_searching/peaks_valleys.rb
@@ -9,8 +9,40 @@
 # Output: [5,1,3,2,3]
 
 def peaks_valleys(array)
-  prev_num = nil
-  peak = array[0] > array[1]
-  array.each_index do |idx|
+  sorted_array = array.sort
+  last_idx = array.length-1
+  idx = 0
+  result = []
+  while idx < last_idx
+    result.push(sorted_array[last_idx])
+    result.push(sorted_array[idx])
+    idx += 1
+    last_idx -= 1
   end
+
+  result
+end
+
+
+def sort_valley_peak(array)
+  idx = 1
+  while idx < array.length-1
+    biggest_idx = max_index(array, idx-1, idx, idx + 1)
+    idx += 2
+    if idx != biggest_idx
+      swap(array, idx, biggest_idx)
+    end
+  end
+  array
+end
+
+def swap(array, idx, idx2)
+  array[idx], array[idx2] = array[idx2], array[idx]
+end
+
+def max_index(array, idx1, idx2, idx3)
+  max_num = [array[idx1], array[idx2], array[idx3]].max
+  return idx1 if max_num == array[idx1]
+  return idx2 if max_num == array[idx2]
+  return idx2 if max_num == array[idx3]
 end
