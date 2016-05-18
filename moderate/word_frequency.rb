a7b10f76b4dc2c36fe789a5cbb0792026994f0c9
stevejk31 <stevekim31@gmail.com>
2016-05-18 12:29:08 -0700

    single look up word frequency in a book

--- moderate/word_frequency.rb
+++ moderate/word_frequency.rb
@@ -2,22 +2,20 @@
 # What if we were running this algorithm mutiple times
 
 def read_book
-
+  File.readlines('book.txt').map {|line| line.chomp}
 end
 
-def word_counter
-  word_hash = Hash.new { |hash, key| hash[key] = 0 }
+# non-persistent single look up word frequency
+def word_frequency(input_word)
+  count = 0
   read_book.each do |line|
     words = line.split(" ")
     words.each do |word|
       word = word.gsub(/\W+/, '')
       word = word.downcase
-      word_hash[word] += 1
+      count +=1 if word == input_word
     end
   end
-end
 
-
-def word_frequency(word)
-  word_hash[word]
+  count
 end
