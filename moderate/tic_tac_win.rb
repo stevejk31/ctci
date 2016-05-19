2c97e5ce6802b024b89d7cdfafd9976200ee7084
stevejk31 <stevekim31@gmail.com>
2016-05-19 13:25:30 -0700

    tic tac toe win solution horizontal, vertical, and diagonal

--- moderate/tic_tac_win.rb
+++ moderate/tic_tac_win.rb
@@ -10,6 +10,24 @@
 
 def tic_tac_win(board)
   #horizontal
+  board.each do |row|
+    ["x","o"].each do |player|
+      return player if row.all? {|el| el == player}
+    end
+  end
   #vertical
-  #diagonal
+  3.times do |col|
+    ["x","o"].each do |player|
+      return player if board.all? {|row| row[col] == player}
+    end
+  end
+  #diagonal top-left to bottom-right
+  ["x","o"].each do |player|
+    return player if board[0][0] == player && board[1][1] == player && board[2][2] == player
+  end
+  #diagnoal top-right to bottom-left
+  ["x","o"].each do |player|
+    return player if board[2][0] == player && board[1][1] == player && board[0][2] == player
+  end
+  false
 end
