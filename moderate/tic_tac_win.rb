1b5fe713b10408510bec50f9e3d4bc06292151e5
stevejk31 <stevekim31@gmail.com>
2016-05-19 13:18:14 -0700

    start tic_tac_win

---
+++ moderate/tic_tac_win.rb
@@ -0,0 +1,15 @@
+# Design an algoritm to figure out if someone has won a game of tic-tac-toe
+
+# Input:
+# [
+#   ["x","x","x"],
+#   ["o",nil,"o"],
+#   ["o","x","o"]
+# ]
+
+
+def tic_tac_win(board)
+  #horizontal
+  #vertical
+  #diagonal
+end
