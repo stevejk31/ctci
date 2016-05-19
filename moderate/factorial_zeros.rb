0f10fe7b8e2dbed3aff35236d2d1cf4b5f68d3bb
stevejk31 <stevekim31@gmail.com>
2016-05-19 14:34:07 -0700

    create brute force trailing zero in factorial solution

--- moderate/factorial_zeros.rb
+++ moderate/factorial_zeros.rb
@@ -1,1 +1,25 @@
 # Write an algorithm which computes the number of trailing zeros in n factorial.
+
+def factorial(n)
+  return [] if n == 0
+  return [1] if n == 1
+  return [1,2] if n == 2
+  prev_factorial = factorial(n-1)
+  prev_factorial.push(prev_factorial.last*n)
+end
+
+def bf_trailing_zero(n)
+  count = 0
+  num = factorial(n).last.to_s.split("")
+  temp = num.pop
+  until temp != "0"
+    count +=1 if temp == "0"
+    temp = num.pop
+  end
+  count
+end
+
+def trailing_zero_factorial(n)
+
+
+end
