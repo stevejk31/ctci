caaf244e57c7d3222fd5b172aff8f15f8f670257
stevejk31 <stevekim31@gmail.com>
2016-05-24 11:24:36 -0700

    smallest difference optimized solution

--- moderate/smallest_difference.rb
+++ moderate/smallest_difference.rb
@@ -20,3 +20,24 @@ def smallest_difference(array1, array2)
   end
   smallest_diff
 end
+
+
+def small_diff_opt(array1, array2)
+  array1.sort!
+  array2.sort!
+  idx1 = 0
+  idx2 = 0
+  min_diff = (array1.first - array2.first).abs
+  while idx1 < array1.length && idx2 < array2.length
+    num1 = array1[idx1]
+    num2 = array2[idx2]
+    current_diff = (num1-num2).abs
+    min_diff = current_diff if min_diff > current_diff
+    if num1 < num2
+      idx1 += 1
+    else
+      idx2 += 1
+    end
+  end
+  min_diff
+end
