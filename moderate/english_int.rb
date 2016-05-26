26d7df301c96ace064ac700ae5d0a731f8483e62
stevejk31 <stevekim31@gmail.com>
2016-05-25 18:21:11 -0700

    less than 20 solutions

--- moderate/english_int.rb
+++ moderate/english_int.rb
@@ -1,1 +1,47 @@
 # Given any integer, print an English phrase that describes the integer.
+
+
+ZERO_NINETEEN = {
+  0 => "zero",
+  1 => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four",
+  5 => "five",
+  6 => "six",
+  7 => "seven",
+  8 => "eight",
+  9 => "nine",
+  10 => "ten",
+  11 => "eleven",
+  12 => "twelve",
+  13 => "thirteen",
+  14 => "fourteen",
+  15 => "fifteen",
+  16 => "sixteen",
+  17 => "seventeen",
+  18 => "eighteen",
+  19 => "nineteen"
+}
+
+TENS = {
+  20 => "twenty",
+  30 => "thirty",
+  40 => "fourty",
+  50 => "fifty",
+  60 => "sixty",
+  70 => "seventy",
+  80 => "eighty",
+  90 => "ninety",
+}
+
+def english_int(num)
+  length = num.to_s.length
+  current_length = length % 3
+  current_num = num.to_s.split("").take(current_length).join("")
+
+end
+
+def zero_nineteen(num)
+  ZERO_NINETEEN[num]
+end
