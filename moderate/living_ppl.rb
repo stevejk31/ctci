5374d94eb7b08d26a490cec88a2e603f671faf14
stevejk31 <stevekim31@gmail.com>
2016-06-01 16:23:53 -0700

    instituate niave solution

--- moderate/living_ppl.rb
+++ moderate/living_ppl.rb
@@ -4,7 +4,14 @@
 # that year, they should be included in that year's count. For example, Person
 # (birth = 1908, death = 1909) is included in the counts for both 1908 and 1909
 def most_alive(people)
-
+  ppl_hash = Hash.new { |hash, key| hash[key] = 0 }
+  people.each do |person|
+    (person.birth..person.death).each do |year|
+      ppl_hash[year] += 1
+    end
+  end
+  most_alive_year = hash.max_by{|year, count| count }
+  most_alive_year
 end
 
 class Person
