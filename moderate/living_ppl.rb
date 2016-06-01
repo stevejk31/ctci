43ec73bbf9ee8a002384dda480508e022dd090f2
stevejk31 <stevekim31@gmail.com>
2016-06-01 16:20:51 -0700

    initialize method

--- moderate/living_ppl.rb
+++ moderate/living_ppl.rb
@@ -3,7 +3,9 @@
 # born between 1900 2000(inclusive). If a person was alive during any portion of
 # that year, they should be included in that year's count. For example, Person
 # (birth = 1908, death = 1909) is included in the counts for both 1908 and 1909
+def most_alive(people)
 
+end
 
 class Person
   attr_reader :birth, :death
