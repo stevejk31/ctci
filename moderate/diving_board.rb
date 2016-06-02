99d4cfc16bb9f4ee8bf16e5f9af278729da3458a
stevejk31 <stevekim31@gmail.com>
2016-06-02 12:33:43 -0700

    iterative solution

--- moderate/diving_board.rb
+++ moderate/diving_board.rb
@@ -3,9 +3,25 @@
 # You must use exactly K planks of wood. Write a method to generate all possible
 # lengths for the diving board.
 
-def diving_board(planks)
-
-
+def diving_board(planks, num_planks)
+  shorter = []
+  longer = []
+  planks.each do |plank|
+    if plank.length == :longer
+      longer.push(plank)
+    else
+      shorter.push(shorter)
+    end
+  end
+  result = []
+  longer_planks = longer.length
+  shorter_planks = 0
+  until longer_planks < 0 || shorter_planks > shorter.length
+    result.push([longer_planks, shorter_planks])
+    longer_planks -= 1
+    shorter_planks -= 1
+  end
+  result
 end
 
 
@@ -16,4 +32,13 @@ class Plank
     @length = length
   end
 
+end
+
+planks = []
+10.times do
+  if rand(2) == 1
+    planks.push(Plank.new(:longer))
+  else
+    planks.push(Plank.new(:shorter))
+  end
 end
