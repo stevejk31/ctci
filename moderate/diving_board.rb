bed23b8e83476bceb274cacb224493e4c224c159
stevejk31 <stevekim31@gmail.com>
2016-06-02 12:20:32 -0700

    create plank class

--- moderate/diving_board.rb
+++ moderate/diving_board.rb
@@ -1,4 +1,19 @@
-# You are building a diving board by placing a bunch of planks of wood end-to-end. 
+# You are building a diving board by placing a bunch of planks of wood end-to-end.
 # There are two types of planks, one of length shorter and one of length longer.
 # You must use exactly K planks of wood. Write a method to generate all possible
 # lengths for the diving board.
+
+def diving_board(planks)
+
+
+end
+
+
+class Plank
+  attr_reader :length
+  # length :longer or :shorter
+  def initialize(length)
+    @length = length
+  end
+
+end
