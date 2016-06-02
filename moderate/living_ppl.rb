c98cc0f32b420bf9e2a0b0bdb09ec3fe48251e16
stevejk31 <stevekim31@gmail.com>
2016-06-01 17:49:00 -0700

    solution using hashes and years

--- moderate/living_ppl.rb
+++ moderate/living_ppl.rb
@@ -3,8 +3,9 @@
 # born between 1900 2000(inclusive). If a person was alive during any portion of
 # that year, they should be included in that year's count. For example, Person
 # (birth = 1908, death = 1909) is included in the counts for both 1908 and 1909
-def most_alive(people)
+def most_alive_brute_force(people)
   ppl_hash = Hash.new { |hash, key| hash[key] = 0 }
+  # O(n * m); n = ppl and m = years
   people.each do |person|
     (person.birth..person.death).each do |year|
       ppl_hash[year] += 1
@@ -12,6 +13,29 @@ def most_alive(people)
   end
   most_alive_year = ppl_hash.max_by{|year, count| count }
   most_alive_year.first
+end
+
+def most_alive(people)
+  people.each do |person|
+    births.push(person.birth)
+    deaths.push(person.death)
+  end
+  births.sort!
+  deaths.sort!
+  births_hash = Hash.new { |hash, key| hash[key] = 0 }
+  alive = 0
+  births.each do |year|
+    alive += 1
+    births_hash[year] = alive
+  end
+  deaths_hash = Hash.new { |hash, key| hash[key] = 0 }
+  death.each do |year|
+    death += 1
+    deaths_hash[year] = death
+  end
+  (1900..2000).each do |year|
+
+  end
 end
 
 class Person
