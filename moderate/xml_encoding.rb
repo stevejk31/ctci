3683927190d61aa772d49356902b4f20ded3fc1c
stevejk31 <stevekim31@gmail.com>
2016-06-03 10:03:11 -0700

    first attempt at encoding solution

--- moderate/xml_encoding.rb
+++ moderate/xml_encoding.rb
@@ -13,3 +13,63 @@
 # </family>
 # Becomes:
 # 1 4 McDowell 5 CA 0 2 3 Gayle 0 Some Message 0 0
+blah = <<-BLAH
+<family lastName="McDowell" state="CA">
+  <person firstName="Gayle">Some Message</person>
+</family>
+BLAH
+
+def encoding(xml)
+    nodes = xml.strip.split("<").map(&:strip)
+    nodes.shift
+    result = []
+    nodes.each do |node|
+      if node[0,2] == "/"
+        result.push("0")
+      else
+        result.concat(encoding_line(node))
+      end
+    end
+    result.join(" ")
+end
+
+ATTR = {
+  ">" => 0,
+  "family" => 1,
+  "person" => 2,
+  "firstName" => 3,
+  "lastName" => 4,
+  "state" => 5
+}
+
+# <family lastName="McDowell" state="CA">
+#   <person firstName="Gayle">Some Message</person>
+# </family>
+
+def encoding_line(line)
+  words = []
+  word = ""
+  value = false
+  line.split("").each_with_index do |char, idx|
+    if char == " " || char == "=" || (char == "\"" && value)
+      if value
+        words.push(word)
+        value = false
+      else
+        words.push(ATTR[word])
+      end
+      word = ""
+    elsif char == "\""
+      p "it's a value"
+      value = true
+    elsif char == ">"
+      words.push(line[idx+1..-1])
+      break
+    else
+      word += char
+    end
+  end
+  words
+end
+
+encoding(blah)
