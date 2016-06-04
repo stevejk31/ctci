57445b5c4976168acab245a34e87c8f418010c90
stevejk31 <stevekim31@gmail.com>
2016-06-04 15:58:22 -0700

    calc mean

--- moderate/best_line.rb
+++ moderate/best_line.rb
@@ -2,7 +2,19 @@
 # most number of points.
 
 def best_line(points)
+  mean_x, mean_y = mean(points)
 
 
+end
+
+
+def mean(points)
+  x_total = 0
+  y_total = 0
+  points.each do |point|
+    x_total += point[0]
+    y_total += point[1]
+  end
+  [x_total / points.length, y_total / points.length]
 
 end
