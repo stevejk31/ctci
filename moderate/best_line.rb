00b28c065803f6cb1cd1b4506414ff60f44a268b
stevejk31 <stevekim31@gmail.com>
2016-06-04 16:02:55 -0700

    calc slope function

--- moderate/best_line.rb
+++ moderate/best_line.rb
@@ -3,7 +3,7 @@
 
 def best_line(points)
   mean_x, mean_y = mean(points)
-
+  slope = find_slope(mean_x, mean_y, points)
 
 end
 
@@ -16,5 +16,17 @@ def mean(points)
     y_total += point[1]
   end
   [x_total / points.length, y_total / points.length]
+
+end
+
+
+def find_slope(mean_x, mean_y, points)
+  top = 0
+  bottom = 0
+  points.each do |point|
+    top += ((point[0] - mean_x)*(point[1] - mean_y))
+    bottom += (point[0] - mean_x)**2
+  end
+  (top / bottom)
 
 end
