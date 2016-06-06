b5bd436a7061c3f9c7520821502ce20d9def0b97
stevejk31 <stevekim31@gmail.com>
2016-06-06 12:02:52 -0700

    start sub sort problem

---
+++ moderate/sub_sort.rb
@@ -0,0 +1,12 @@
+# Given an array of integers, write a method to find indices m and n such that
+# if you sorted elements m through n, the entire array would be sorted. Minimize
+# n - m (that is, find the smallest such sequence)
+
+# EXAMPLE:
+# Input: 1,2,4,7,10,11,7,12,6,7,16,18,19
+# Output: (3,9)
+
+def sub_sort(array)
+
+
+end
