b5884e2fbae54130f3922da83474c3c48a6e91d1
stevejk31 <stevekim31@gmail.com>
2016-06-06 13:51:12 -0700

    indexing problem

--- moderate/sub_sort.rb
+++ moderate/sub_sort.rb
@@ -9,26 +9,28 @@
 def sub_sort(array)
   left_idx = sorted_left(array)
   right_idx = sorted_right(array)
+  p [left_idx, right_idx]
+  return [left_idx, right_idx] if left_idx == 0 && right_idx == (array.length - 1)
   p array
-  loop do
-    left_array = array[0..left_idx]
-    middle_array = array[(left_idx + 1)..(right_idx - 1)]
-    right_array = array[right_idx..-1]
-    puts "left"
-    p left_array
-    puts "middle"
-    p middle_array
-    puts "right"
-    p right_array
-    if (left_array.last.nil? || left_array.last <= middle_array.min) &&
-      (right_array.first.nil? || right_array.first >= middle_array.max)
-      break
-    end
-    left_idx -= 1 if left_array.last.nil? || left_array.last > middle_array.min
-    right_idx += 1 if right_array.first.nil? || right_array.first < middle_array.max
-  end
-  [left_idx+1, right_idx-1]
+  left_idx = check_left(left_idx, array)
+  right_idx = check_right(right_idx, array)
+  [left_idx + 1, right_idx - 1]
 end
+
+def check_left(left_idx, array)
+  until left_idx == 0 || array[left_idx] < array[left_idx+1..-1].min
+    left_idx -= 1
+  end
+  left_idx
+end
+
+def check_right(right_idx, array)
+  until right_idx == (array.length - 1) || array[right_idx] > array[0..right_idx-1].max
+    right_idx += 1
+  end
+  right_idx
+end
+
 
 def sorted_left(array)
   result = 0
@@ -39,13 +41,13 @@ def sorted_left(array)
   0
 end
 
-
 def sorted_right(array)
   length = array.length - 1
   result = length
   (length).times do |idx|
+    p result
     return result if array[length - idx] < array[length - idx - 1]
     result = length - idx
   end
-  result
+  length
 end
