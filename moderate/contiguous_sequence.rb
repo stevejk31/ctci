8acc8940ef8a340cd3f510b420fb8b6fdd9be4bc
stevejk31 <stevekim31@gmail.com>
2016-06-07 09:55:31 -0700

    start contiguous sequence problem

---
+++ moderate/contiguous_sequence.rb
@@ -0,0 +1,10 @@
+# You are given an array integers (both positive and negative). Find the
+# contiguous sequence with the largest sum.
+#
+# Example:
+# Input: [2, -8, 3, -2, 4, -10]
+# Output: 5 (i.e., [4,-2,4])
+
+def contiguous_sequence(array)
+
+end
