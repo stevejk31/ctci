b664533f7c0cb529c7ef9631c0914faa63683a5d
stevejk31 <stevekim31@gmail.com>
2016-06-07 10:31:35 -0700

    start pattern matching problem

---
+++ moderate/pattern_matching.rb
@@ -0,0 +1,9 @@
+# You are given two strings, pattern and value. The pattern string consists of
+# just the letters of a and b, describing a pattern within a string. For example,
+# the string catcatgocatgo matches the pattern aabab(where cat is a and go is b).
+# It also matches patterns like a, ab, and b. Write a method ot determine if a
+# value matches pattern.
+
+def pattern_matching(pattern, value)
+
+end
