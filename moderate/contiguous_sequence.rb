dc830a361b775d1305e8c6ed799c7de0223f177b
stevejk31 <stevekim31@gmail.com>
2016-06-07 10:00:29 -0700

    nested loop solution

--- moderate/contiguous_sequence.rb
+++ moderate/contiguous_sequence.rb
@@ -6,5 +6,14 @@
 # Output: 5 (i.e., [4,-2,4])
 
 def contiguous_sequence(array)
-
+  sum_max = array.first
+  array.each_with_index do |num, idx|
+    current_sum = num
+    array[(idx+1)..-1].each do |num|
+      current_sum += num
+      sum_max = current_sum if current_sum > sum_max
+    end
+  end
+  
+  sum_max
 end
