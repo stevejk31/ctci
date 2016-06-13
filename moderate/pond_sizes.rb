70a11339f07c3bca82fafb833fead61f216bf0e7
stevejk31 <stevekim31@gmail.com>
2016-06-13 11:31:49 -0700

    recursive pond finder)

--- moderate/pond_sizes.rb
+++ moderate/pond_sizes.rb
@@ -14,3 +14,31 @@
 # ]
 #
 # Output 2, 4, 1 in any order
+
+def pond_size(matrix)
+  ponds = []
+  found = Hash.new { |hash, key| hash[key] = false }
+  matrix.each do |array|
+    array.each do |el|
+
+    end
+  end
+
+  ponds
+end
+
+def pond(initial, matrix)
+  row, col = initial
+  return 0 if matrix[row][col] != 0
+  above = pond([row-1, col], matrix)
+  below = pond([row+1, col], matrix)
+  left = pond([row, col-1], matrix)
+  right = pond(row, col+1], matrix)
+
+
+end
+
+def in_bounds?(pos, matrix)
+
+
+end
