f9fdfa3e199260ba74d12a3cd8a6cc759532d50b
stevejk31 <stevekim31@gmail.com>
2016-06-16 14:38:26 -0700

    take initial situations where computation unnecessary or not possible

--- moderate/sum_swap.rb
+++ moderate/sum_swap.rb
@@ -5,5 +5,13 @@
 # Input: [4,1,2,1,1,2] [3,6,3,3]
 # Output: [1,3]
 
-def sum_swap(array1, array2
+def sum_swap(array1, array2)
+  array1.sort!
+  array2.sort!
+  sum1 = array1.inject {|sum, num| sum + num}
+  sum2 = array2.inject {|sum, num| sum + num}
+  diff = (sum1 - sum2).abs / 2
+  return [] if sum1 == sum2
+  return false unless (sum1 - sum2) % 2 == 0
+
 end
