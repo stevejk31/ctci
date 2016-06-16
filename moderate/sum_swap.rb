9e5bc022d47853c9c2828fb24454d620ae96c381
stevejk31 <stevekim31@gmail.com>
2016-06-16 14:49:07 -0700

    binary search solution to sum swap

--- moderate/sum_swap.rb
+++ moderate/sum_swap.rb
@@ -2,16 +2,38 @@
 # that you can swap to give the two arrays the same sum.
 
 # EXAMPLE:
-# Input: [4,1,2,1,1,2] [3,6,3,3]
+# Input:
+# array1 = [4,1,2,1,1,2]
+# array2 = [3,6,3,3]
 # Output: [1,3]
 
 def sum_swap(array1, array2)
-  array1.sort!
-  array2.sort!
   sum1 = array1.inject {|sum, num| sum + num}
   sum2 = array2.inject {|sum, num| sum + num}
-  diff = (sum1 - sum2).abs / 2
   return [] if sum1 == sum2
   return false unless (sum1 - sum2) % 2 == 0
+  diff = (sum1 - sum2) / 2
+  array2.sort!
+  array1.each do |num|
+    return [num, num - diff] if binary_search(array2, num - diff)
+  end
+  false
+end
 
+
+
+def binary_search(array, target)
+  return nil if array.length == 0
+  mid_idx = array.length/2
+  mid_value = array[mid_idx]
+  left_array = array.take(mid_idx)
+  right_array = array.drop(mid_idx+1)
+  if target < mid_value
+    binary_search(left_array, target)
+  elsif target == mid_value
+    mid_idx
+  else
+    right = binary_search(right_array, target)
+    mid_idx + 1 + right if right
+  end
 end
