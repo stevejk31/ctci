840461b0edaab8faef5f890d0c34c943696e88fb
stevejk31 <stevekim31@gmail.com>
2016-06-16 09:37:28 -0700

    initial solution to pond size using recursion

--- moderate/pond_sizes.rb
+++ moderate/pond_sizes.rb
@@ -12,15 +12,16 @@
 #   [1,1,0,1],
 #   [0,1,0,1]
 # ]
-#
+# a = [[0,2,1,0],[0,1,0,1],[1,1,0,1],[0,1,0,1]]
 # Output 2, 4, 1 in any order
 
 def pond_size(matrix)
   ponds = []
   found = Hash.new { |hash, key| hash[key] = false }
-  matrix.each do |array|
-    array.each do |el|
-
+  matrix.each_with_index do |array, row|
+    array.each_index do |col|
+      count = pond([row,col], matrix)
+      ponds.push(count) if count > 0
     end
   end
 
@@ -29,16 +30,20 @@ end
 
 def pond(initial, matrix)
   row, col = initial
+  return 0 unless !!matrix[row] && !!matrix[row][col] && in_bounds?(initial, matrix)
   return 0 if matrix[row][col] != 0
-  above = pond([row-1, col], matrix)
-  below = pond([row+1, col], matrix)
-  left = pond([row, col-1], matrix)
-  right = pond(row, col+1], matrix)
-
-
+  matrix[row][col] = nil
+  count = 0
+  (row-1..row+1).each do |cur_row|
+    (col-1..col+1).each do |cur_col|
+      count+= pond([cur_row,cur_col],matrix)
+    end
+  end
+  count + 1
 end
 
 def in_bounds?(pos, matrix)
-
+  row, col = pos
+  !!row && !!col && row < matrix.length && col < matrix[0].length && row >= 0 && col >= 0
 
 end
